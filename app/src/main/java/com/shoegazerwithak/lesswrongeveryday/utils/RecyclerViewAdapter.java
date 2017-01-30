e26391589bb8a2de10e555f30d5b491b0db17a1f
shoegazerwithak <skrtroot@protonmail.com>
2017-01-30 16:29:16 +0300

    RecyclerView with onClick, working

--- app/src/main/java/com/shoegazerwithak/lesswrongeveryday/utils/RecyclerViewAdapter.java
+++ app/src/main/java/com/shoegazerwithak/lesswrongeveryday/utils/RecyclerViewAdapter.java
@@ -21,31 +21,20 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     View view1;
     ViewHolder viewHolder1;
     TextView textView;
-    
-    private AdapterView.OnItemClickListener onItemClickListener;
 
-    public RecyclerViewAdapter(Context context1, List<Map<String, String>> SubjectValues1, AdapterView.OnItemClickListener onItemClickListener) {
+//    private AdapterView.OnItemClickListener onItemClickListener;
+
+    public RecyclerViewAdapter(Context context1, List<Map<String, String>> SubjectValues1) {
         SubjectValues = SubjectValues1;
         context = context1;
-        this.onItemClickListener = onItemClickListener;
+//        this.onItemClickListener = onItemClickListener;
     }
 
     @Override
     public RecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         view1 = LayoutInflater.from(context).inflate(R.layout.recyclerview_items, parent, false);
-        view1.setOnClickListener(new MyOnClickListener());
-
         viewHolder1 = new ViewHolder(view1);
-
         return viewHolder1;
-    }
-
-    private class MyOnClickListener implements View.OnClickListener {
-        @Override
-        public void onClick(View view) {
-            //passing the clicked position to the parent class
-            onItemClickListener.onItemClick(null, view, getAdapterPosition(), view.getId());
-        }
     }
 
     @Override
@@ -59,13 +48,34 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return SubjectValues.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder {
+//    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener  {
         public TextView textView;
 
-        public ViewHolder(View v) {
-            super(v);
-
-            textView = (TextView) v.findViewById(R.id.subject_textview);
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.subject_textview);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(context, SubjectValues.get(getLayoutPosition()).get("link"), Toast.LENGTH_LONG).show();
+                    Log.e("Item Click Position", String.valueOf(getLayoutPosition()));
+                }
+            });
         }
+
+//        public ViewHolder(View itemView) {
+//            super(itemView);
+//            textView = (TextView) itemView.findViewById(R.id.subject_textview);
+//
+////            textView.setOnClickListener(this);
+////            itemView.setOnClickListener(this);
+//        }
+
+//        @Override
+//        public void onClick(View view) {
+//            passing the clicked position to the parent class
+//            onItemClickListener.onItemClick(null, view, getAdapterPosition(), view.getId());
+//        }
     }
 }
